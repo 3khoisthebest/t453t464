c699124055a06f858a84b0c506d3a51ec910b875
3khoisthebest <aaqilnazeem.mohammedibrahim.404@k12.friscoisd.org>
2024-01-17 15:47:52 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ public/js/play.js
@@ -0,0 +1,20 @@
+// Hey skid or contributor.
+// This script Fetches games.json.
+document.addEventListener("DOMContentLoaded", function () {
+  const urlParams = new URLSearchParams(window.location.search);
+  const gameUrl = urlParams.get("game");
+
+  fetch("/games.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const gameData = data.find((game) => game.url === gameUrl);
+
+      document.title = `${gameData.name} | Nativegames`;
+      document.getElementById("gamename").innerText = gameData.name;
+      document.getElementById("favicon").href = gameData.image;
+      document.getElementById(
+        "gameIframe"
+      ).src = `https://3kh0-assets-topaz.vercel.app/${gameUrl}/`;
+    })
+    .catch((error) => console.error("Error fetching game data:", error));
+});
